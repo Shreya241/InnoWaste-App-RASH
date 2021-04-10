49412f1a046665729ba274e2c1aabbd49b4a8a4a
Henit Chobisa <henit.chobisa2020@vitstudet.ac.in>
2021-04-10 18:34:53 +0530

    Project Set UP

---
+++ TeamRash_InnoWaste/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TeamRash_InnoWaste
+//
+//  Created by Henit Work on 10/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
